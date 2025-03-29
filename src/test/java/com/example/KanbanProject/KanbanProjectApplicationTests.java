9230ed05826a18581024d23d9bac297c4a48258e
Martynas <martis.jascemskas@gmail.com>
2025-03-29 20:48:29 +0000

    init of kanban backend and database

---
+++ src/test/java/com/example/KanbanProject/KanbanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KanbanProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
